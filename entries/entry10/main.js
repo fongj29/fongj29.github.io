956881cdfb3ef98903033c990657c81cd0786267
fongj29 <fongj@newschool.edu>
2023-11-21 19:23:16 -0500

    Entry 10 button
    Button now toggles image to appear by using ID to target it. Added javascript to do hide/show.
    
    Media query is still having issues.

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -7,18 +7,21 @@ let body = document.body;
 
 function buttonOn() {
   var button = document.querySelector(".button");
-  var card = document.querySelector(".carddrop"); 
+  var card = document.getElementById("card"); 
+  var front = document.getElementById("postcardfront"); 
 
   button.classList.toggle("buttonOn"); 
 
   if (button.classList.contains("buttonOn")) {
     button.innerHTML = '<img src="imgs/postopening.png">';
-    card.classList.remove('nocard');
+    front.style.display = 'block';
+    // card.classList.remove('nocard');
     card.classList.add('carddrop');
   } else {
     button.innerHTML = 'post output';
     card.classList.remove('carddrop');
-    card.classList.add('nocard');
+    // card.classList.add('nocard');
+    front.style.display = 'none';
   }
 }
 
