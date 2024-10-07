f2f5e8a98d4abffbabbbe3db0d99c77eafcbc130
fongj29 <fongj@newschool.edu>
2024-10-07 03:30:16 -0400

    Push 6
    Fixed the title to the bottom left. Changed the icon colors to grayscale but retail in green. Pushing to see if it works

--- Core3/PointOfEntry/script.js
+++ Core3/PointOfEntry/script.js
@@ -1,17 +1,14 @@
-// This function initializes the map
 function initMap() {
-    var location = { lat: 40.73356628417969, lng: -73.98146057128906 };  // Marker position
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: location
-    });
+  var nyc = { lat: 40.7128, lng: -74.0060 };  // Coordinates for New York City
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: nyc
+  });
 
-    // Create a marker
-    var marker = new google.maps.marker.AdvancedMarkerElement({
-        map: map,
-        position: location,
-        title: "My Location"
-    });
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+    map: map,
+    position: nyc
+  });
 }
 
   console.log ("This is the Point.");
