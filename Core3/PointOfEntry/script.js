52f66a6e13c7e70c54d285de34723c768c52395b
fongj29 <fongj@newschool.edu>
2024-10-07 01:55:32 -0400

    Push 4
    Trying a window maps with new unrestricted API.

--- Core3/PointOfEntry/script.js
+++ Core3/PointOfEntry/script.js
@@ -1,14 +1,15 @@
 function initMap() {
-    var nyc = { lat: 40.7128, lng: -74.0060 };  // Coordinates for New York City
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: nyc
-    });
-  
-    var marker = new google.maps.Marker({
-      position: nyc,
-      map: map
-    });
-  }
+  var nyc = { lat: 40.7128, lng: -74.0060 };  // Coordinates for New York City
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: nyc
+  });
+
+  const marker = new google.maps.marker.AdvancedMarkerElement({
+    map: map,
+    position: nyc
+  });
+}
+
   console.log ("This is the Point.");
   
