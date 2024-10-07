ad345998f53712165f51220798afe64526fb1512
fongj29 <fongj@newschool.edu>
2024-10-07 01:02:38 -0400

    Push 2
    Trying to make the API work to get the map to activate. Got the Verdana Pro font to work in the headline.

--- Core3/PointOfEntry/script.js
+++ Core3/PointOfEntry/script.js
@@ -1,7 +1,13 @@
-
-  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyD9By8Chcuj0GdEhBnaiTd4BmAFRWo3QMM",
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-  });
+function initMap() {
+    var nyc = { lat: 40.7128, lng: -74.0060 };  // Coordinates for New York City
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: nyc
+    });
+  
+    var marker = new google.maps.Marker({
+      position: nyc,
+      map: map
+    });
+  }
+  
